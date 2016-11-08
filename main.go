37d86a62cf4bb8299dbd4f555c9e33f5f99d9e31
pgrunde <peter.a.grunde@gmail.com>
2016-11-07 23:11:20 -0700

    html template separated

--- main.go
+++ main.go
@@ -2,22 +2,15 @@ package main
 
 import (
 	"fmt"
-	"net/http"
 	"html"
+	"html/template"
+	"net/http"
 )
 
-// This the template for our root page
-const template = `<!DOCTYPE html>
-<html>
-	<head>
-		<link rel="stylesheet" href="public/css/main.css">
-		<script src="public/js/main.js"></script>
-	</head>
-	<body>
-		<h2>Thanks for visiting my path %q</h2>
-	</body>
-</html>
-`
+type Home struct {
+	Path string
+	Name string
+}
 
 // main function runs the app
 func main() {
@@ -35,6 +28,11 @@ func main() {
 // root is the handler function, it tells me what to do when a user visits root
 func root(w http.ResponseWriter, r *http.Request) {
 	w.Header().Add("content-type", "text/html; charset=utf-8")
-	fmt.Fprintf(w, template, html.EscapeString(r.URL.Path))
+	data := Home{
+		Path: html.EscapeString(r.URL.Path),
+		Name: "Grunde",
+	}
+	t, _ := template.ParseFiles("templates/home.html")
+	t.Execute(w, data)
 	return
 }
