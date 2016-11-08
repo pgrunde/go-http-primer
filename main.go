a000ec3b94f3d2fdf8064c1f063c2fe6cc3fbaf1
pgrunde <peter.a.grunde@gmail.com>
2016-11-07 23:27:52 -0700

    url values fetched

--- main.go
+++ main.go
@@ -10,6 +10,7 @@ import (
 type Home struct {
 	Path string
 	Name string
+	Color string
 }
 
 // main function runs the app
@@ -28,9 +29,14 @@ func main() {
 // root is the handler function, it tells me what to do when a user visits root
 func root(w http.ResponseWriter, r *http.Request) {
 	w.Header().Add("content-type", "text/html; charset=utf-8")
+	color := r.URL.Query().Get("color")
+	if color == "" {
+		color = "black"
+	}
 	data := Home{
 		Path: html.EscapeString(r.URL.Path),
 		Name: "Grunde",
+		Color: color,
 	}
 	t, _ := template.ParseFiles("templates/home.html")
 	t.Execute(w, data)
